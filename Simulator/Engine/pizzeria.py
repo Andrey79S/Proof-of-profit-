faab6f639640801b5f95b27f6b6644bfda9253bb
Andrey79S <phoenixbrite@gmail.com>
2026-01-01 14:06:53 +0200

    Update pizzeria.py

--- Simulator/Engine/pizzeria.py
+++ Simulator/Engine/pizzeria.py
@@ -5,26 +5,29 @@ from engine.energy import Energy
 class Pizzeria:
     def __init__(self):
         self.production = Production()
-        self.energy = Energy()
+        self.energy = Energy(working_hours=12)
 
     def simulate_day(self, margarita_qty, pepperoni_qty):
-        production_cost = self.production.make_pizzas(
-            margarita_qty,
-            pepperoni_qty
+        dough_kg = self.production.total_dough_required(
+            margarita_qty, pepperoni_qty
         )
 
-        energy_cost = self.energy.calculate_daily_energy(
-            margarita_qty + pepperoni_qty
+        ingredient_cost = self.production.make_pizzas(
+            margarita_qty, pepperoni_qty
         )
+
+        energy_cost = self.energy.total_energy(dough_kg)
 
         revenue = margarita_qty * 10 + pepperoni_qty * 14
-        total_cost = production_cost + energy_cost
+        total_cost = ingredient_cost + energy_cost
         profit = revenue - total_cost
 
         return {
-            "revenue": revenue,
-            "production_cost": round(production_cost, 2),
+            "pizzas_total": margarita_qty + pepperoni_qty,
+            "dough_used_kg": round(dough_kg, 2),
+            "ingredient_cost": round(ingredient_cost, 2),
             "energy_cost": round(energy_cost, 2),
             "total_cost": round(total_cost, 2),
+            "revenue": revenue,
             "net_profit": round(profit, 2)
         }
