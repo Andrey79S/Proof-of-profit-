814da31b887158728ad03fe271464c36c94c6eab
Andrey79S <phoenixbrite@gmail.com>
2026-01-05 17:49:34 +0200

    Update clock.py

--- Simulator/core/clock.py
+++ Simulator/core/clock.py
@@ -1,6 +1,18 @@
+# core/clock.py
 class Clock:
     def __init__(self):
-        self.now = 0  # минуты от старта
+        self.day = 1
+        self.minute = 0  # минута в дне
+        self.total_minutes = 0
 
-    def advance(self, minutes: int):
-        self.now += minutes
+    def advance(self, minutes):
+        self.minute += minutes
+        self.total_minutes += minutes
+        while self.minute >= 1440:  # 24*60 минут в дне
+            self.minute -= 1440
+            self.day += 1
+
+    def current_time(self):
+        h = self.minute // 60
+        m = self.minute % 60
+        return f"День {self.day}, {h:02d}:{m:02d}"
