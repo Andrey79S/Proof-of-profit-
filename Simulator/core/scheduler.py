70bc14b2c58504a7db41d85d6b418dcea6041b72
Andrey79S <phoenixbrite@gmail.com>
2026-01-05 18:20:09 +0200

    Update scheduler.py

--- Simulator/core/scheduler.py
+++ Simulator/core/scheduler.py
@@ -1,14 +1,15 @@
-# core/scheduler.py
-import heapq
-
 class Scheduler:
     def __init__(self):
-        self.events = []
+        self.queue = []  # список (time, callback)
+        self.now = 0
 
-    def schedule(self, event):
-        heapq.heappush(self.events, (event.time, event))
+    def schedule(self, time: int, callback):
+        self.queue.append((time, callback))
 
-    def run(self, until_minute):
-        while self.events and self.events[0][0] <= until_minute:
-            _, event = heapq.heappop(self.events)
-            event.execute()
+    def tick(self):
+        """Выполнить события на текущую минуту"""
+        for event in list(self.queue):
+            if event[0] <= self.now:
+                event[1]()  # вызываем callback
+                self.queue.remove(event)
+        self.now += 1
