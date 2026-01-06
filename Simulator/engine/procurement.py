fc8a5216514e3fc17564d70bc1f3d7416c551c97
Andrey79S <phoenixbrite@gmail.com>
2026-01-06 17:02:11 +0200

    Update procurement.py

--- Simulator/engine/procurement.py
+++ Simulator/engine/procurement.py
@@ -2,32 +2,10 @@
 import random
 
 class Procurement:
-    """
-    Логика закупки ингредиентов
-    """
-    def __init__(self, pizzeria, delivery_time_min=1):
+    def __init__(self, pizzeria):
         self.pizzeria = pizzeria
-        self.delivery_time_min = delivery_time_min
-        self.orders = []  # список заказов на поставку
 
-    def order_ingredient(self, name, amount, now):
-        """
-        Создаём заказ на поставку
-        """
-        self.orders.append({
-            "ingredient": name,
-            "amount": amount,
-            "arrives_at": now + self.delivery_time_min
-        })
-
-    def process_orders(self, now):
-        """
-        Проверяем, пришли ли поставки
-        """
-        delivered = []
-        for o in self.orders:
-            if o["arrives_at"] <= now:
-                self.pizzeria.inventory.add_product(o["ingredient"], o["amount"])
-                delivered.append(o)
-        for o in delivered:
-            self.orders.remove(o)
+    def order_ingredients(self, ingredient_list):
+        # доставляем через фиксированное время (1 минута в игре)
+        for name, qty in ingredient_list.items():
+            self.pizzeria.inventory.add(name, qty)
