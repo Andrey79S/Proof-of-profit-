179f7b40e3d837dd05bdeacbe83048cf26d5b901
Andrey79S <phoenixbrite@gmail.com>
2026-01-06 13:55:45 +0200

    Update product.py

--- Simulator/domain/product.py
+++ Simulator/domain/product.py
@@ -1,10 +1,18 @@
-# domain/product.py
+class Ingredient:
+    def __init__(self, name, quantity=0, spoil_time=None):
+        self.name = name
+        self.quantity = quantity
+        self.spoil_time = spoil_time  # минуты до порчи
+        self.added_at = 0
 
-from enum import Enum
+    def is_spoiled(self, current_time):
+        if self.spoil_time is None:
+            return False
+        return (current_time - self.added_at) >= self.spoil_time
 
+class Dough(Ingredient):
+    pass
 
-class ProductState(Enum):
-    RAW = "raw"
-    PROOFING = "proofing"
-    READY = "ready"
-    SPOILED = "spoiled"
+class Pizza(Ingredient):
+    def __init__(self, recipe, quantity=1):
+        super().__init__(name=recipe, quantity=quantity)
