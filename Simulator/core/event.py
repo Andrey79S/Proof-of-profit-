1fce7a3924bd739dbc80a4f2cd23d3c69fa41fab
Andrey79S <phoenixbrite@gmail.com>
2026-01-06 16:26:29 +0200

    Update event.py

--- Simulator/core/event.py
+++ Simulator/core/event.py
@@ -1,9 +1,9 @@
+# core/event.py
 class Event:
-    def __init__(self, time, callback, *args, **kwargs):
+    def __init__(self, time, callback, description=""):
         self.time = time
         self.callback = callback
-        self.args = args
-        self.kwargs = kwargs
+        self.description = description
 
-    def run(self):
-        self.callback(*self.args, **self.kwargs)
+    def trigger(self):
+        self.callback()
