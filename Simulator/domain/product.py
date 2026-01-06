5768b3f2b4ff4326aa4590c26383041db3c42727
Andrey79S <phoenixbrite@gmail.com>
2026-01-06 14:24:33 +0200

    Update product.py

--- Simulator/domain/product.py
+++ Simulator/domain/product.py
@@ -1,18 +1,7 @@
-class Ingredient:
-    def __init__(self, name, quantity=0, spoil_time=None):
+class Product:
+    def __init__(self, name: str, amount: float):
         self.name = name
-        self.quantity = quantity
-        self.spoil_time = spoil_time  # минуты до порчи
-        self.added_at = 0
+        self.amount = amount  # кг или шт
 
-    def is_spoiled(self, current_time):
-        if self.spoil_time is None:
-            return False
-        return (current_time - self.added_at) >= self.spoil_time
-
-class Dough(Ingredient):
-    pass
-
-class Pizza(Ingredient):
-    def __init__(self, recipe, quantity=1):
-        super().__init__(name=recipe, quantity=quantity)
+    def __repr__(self):
+        return f"{self.name}: {self.amount:.2f}"
