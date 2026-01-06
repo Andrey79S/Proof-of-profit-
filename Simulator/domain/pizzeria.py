21f8540e071fe04550308d87c627e759e7ad3f78
Andrey79S <phoenixbrite@gmail.com>
2026-01-06 16:31:48 +0200

    Update pizzeria.py

--- Simulator/domain/pizzeria.py
+++ Simulator/domain/pizzeria.py
@@ -1,14 +1,17 @@
+# domain/pizzeria.py
 class Pizzeria:
-    def __init__(self, inventory, equipment, staff):
+    def __init__(self, equipment_list, staff_list, inventory):
+        self.equipment = equipment_list
+        self.staff = staff_list
         self.inventory = inventory
-        self.equipment = equipment
-        self.staff = staff
-
-        self.money = 0.0
-        self.energy_used = 0.0
-
-        from engine.production import Production
-        self.production = Production(self)
+        self.production = None  # подключается engine.production
 
     def can_accept_order(self, order):
-        return self.production.can_cook(order.recipe)
+        # проверяем ингредиенты
+        try:
+            for ing, amt in order.recipe["ingredients"].items():
+                self.inventory.consume(ing, 0)  # проверка
+        except:
+            return False
+        # проверка staff/equipment
+        return True
