90af1607f002db1bdf374b4bbf10d7cbaaa7031a
Andrey79S <phoenixbrite@gmail.com>
2026-01-06 14:20:11 +0200

    Update pizzeria.py

--- Simulator/domain/pizzeria.py
+++ Simulator/domain/pizzeria.py
@@ -1,9 +1,12 @@
+from engine.production import Production
+
 class Pizzeria:
     def __init__(self, inventory, equipment_list, staff_list):
         self.inventory = inventory
         self.equipment = equipment_list
         self.staff = staff_list
 
+        self.production = Production(self)
+
     def can_accept_order(self, order):
-        # Заглушка: проверка ресурсов
         return True
