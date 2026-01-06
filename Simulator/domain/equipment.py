344025aab3c568d9e3f82d041f1ce3c3443ba240
Andrey79S <phoenixbrite@gmail.com>
2026-01-06 14:30:29 +0200

    Update equipment.py

--- Simulator/domain/equipment.py
+++ Simulator/domain/equipment.py
@@ -1,17 +1,11 @@
 class Equipment:
-    def __init__(self, name, power_kw=0):
+    def __init__(self, name, power_kw):
         self.name = name
         self.power_kw = power_kw
-        self.in_use = False
 
-    def use(self, duration_min):
-        self.in_use = True
-
-    def release(self):
-        self.in_use = False
 
 class Oven(Equipment):
-    def __init__(self, name, power_kw, capacity, bake_time):
+    def __init__(self, name, power_kw, capacity, cook_time):
         super().__init__(name, power_kw)
         self.capacity = capacity
-        self.bake_time = bake_time
+        self.cook_time = cook_time
