2778c3da246a71125ed034afb931def7ac942e03
Andrey79S <phoenixbrite@gmail.com>
2026-01-06 14:28:24 +0200

    Update pizzeria.py

--- Simulator/domain/pizzeria.py
+++ Simulator/domain/pizzeria.py
@@ -1,12 +1,14 @@
-from engine.production import Production
-
 class Pizzeria:
-    def __init__(self, inventory, equipment_list, staff_list):
+    def __init__(self, inventory, equipment, staff):
         self.inventory = inventory
-        self.equipment = equipment_list
-        self.staff = staff_list
+        self.equipment = equipment
+        self.staff = staff
 
+        self.money = 0.0
+        self.energy_used = 0.0
+
+        from engine.production import Production
         self.production = Production(self)
 
     def can_accept_order(self, order):
-        return True
+        return self.production.can_cook(order.recipe)
