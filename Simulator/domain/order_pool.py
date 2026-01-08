22eeee6137186d5aebf52f86da2a3f1d07f27aa9
Andrey79S <phoenixbrite@gmail.com>
2026-01-08 21:14:16 +0200

    Update order_pool.py

--- Simulator/domain/order_pool.py
+++ Simulator/domain/order_pool.py
@@ -1,13 +1,13 @@
-from domain.order import Order
+from domain.order import Order, OrderStatus
 
 class OrderPool:
     def __init__(self):
         self.pool = []
 
-    def add_order(self, recipe, created_at, max_wait=60):
+    def add_order(self, recipe: str, created_at: int, max_wait: int = 60):
         order = Order(recipe, created_at, max_wait)
         self.pool.append(order)
         return order
 
     def pending_orders(self):
-        return [o for o in self.pool if o.status == o.status.PENDING]
+        return [o for o in self.pool if o.status == OrderStatus.PENDING]
