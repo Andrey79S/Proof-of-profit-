103563f547beb0b723738da10b60da512aa4b119
Andrey79S <phoenixbrite@gmail.com>
2026-01-08 19:53:02 +0200

    Update order.py

--- Simulator/domain/order.py
+++ Simulator/domain/order.py
@@ -1,4 +1,3 @@
-# domain/order.py
 from enum import Enum
 
 class OrderStatus(Enum):
@@ -11,17 +10,19 @@ class OrderStatus(Enum):
 class Order:
     _id_counter = 1
 
-    def __init__(self, recipe, created_at, max_wait):
+    def __init__(self, recipe: str, created_at: int, max_wait: int):
         self.id = Order._id_counter
         Order._id_counter += 1
+
         self.recipe = recipe
         self.created_at = created_at
         self.max_wait = max_wait
         self.status = OrderStatus.PENDING
+
         self.accepted_at = None
         self.completed_at = None
 
-    def is_expired(self, now):
+    def is_expired(self, now: int) -> bool:
         return now - self.created_at > self.max_wait
 
     def __repr__(self):
