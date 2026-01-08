e62101951034f280e75ec6f3d06bc34eff98f9de
Andrey79S <phoenixbrite@gmail.com>
2026-01-08 21:10:36 +0200

    Update scheduler.py

--- Simulator/core/scheduler.py
+++ Simulator/core/scheduler.py
@@ -1,16 +1,17 @@
-# core/scheduler.py
 import heapq
+from core.clock import Clock
+from core.event import Event
 
 class Scheduler:
     def __init__(self):
-        self.events = []
+        self.events = []  # Мин-куча: (time, event)
 
-    def add_event(self, event):
+    def add_event(self, event: Event):
         heapq.heappush(self.events, (event.time, event))
 
-    def run_until(self, clock, end_time):
+    def run_until(self, clock: Clock, end_time: int):
         while self.events and clock.now() < end_time:
             event_time, event = heapq.heappop(self.events)
-            if event_time >= clock.now():
+            if event_time > clock.now():
                 clock.tick(event_time - clock.now())
-                event.trigger()
+            event.trigger()
