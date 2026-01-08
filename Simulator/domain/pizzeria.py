3aca2ae38cb54bfbb1e4d9e2810fd2651819b5d9
Andrey79S <phoenixbrite@gmail.com>
2026-01-08 20:06:07 +0200

    Update pizzeria.py

--- Simulator/domain/pizzeria.py
+++ Simulator/domain/pizzeria.py
@@ -1,35 +1,35 @@
+import json
 from domain.equipment import EquipmentFactory
-from domain.staff import Staff
+from domain.staff import StaffFactory
+from domain.product import Recipe
 
 class Pizzeria:
-    def __init__(self, equipment_configs, staff_configs, recipes):
-        self.equipment = {name: EquipmentFactory.create_from_json(path)
-                          for name, path in equipment_configs.items()}
+    def __init__(self, equipment_files, staff_files, recipe_files):
+        # Загружаем оборудование
+        self.equipment = {
+            name: EquipmentFactory.create_from_json(path)
+            for name, path in equipment_files.items()
+        }
+        print("Оборудование загружено:", self.equipment.keys())
 
-        self.staff = {name: Staff(name, skills=cfg.get("skills", {}))
-                      for name, cfg in staff_configs.items()}
+        # Загружаем стафф
+        self.staff = {
+            name: StaffFactory.create_from_json(path)
+            for name, path in staff_files.items()
+        }
+        print("Стафф загружен:", self.staff.keys())
 
-        self.recipes = recipes
-        self.inventory = {ing: 100 for recipe in recipes.values() for ing in recipe["ingredients"]}  # стартовые запасы
-        self.electricity_consumed = 0
-        self.completed_orders = 0
+        # Загружаем рецепты
+        self.recipes = {
+            name: Recipe.from_json(path)
+            for name, path in recipe_files.items()
+        }
+        print("Рецепты загружены:", self.recipes.keys())
+
+        # Инвентарь, тесто и т.д.
+        self.inventory = {}
+        self.production = None  # сюда потом можно поставить ProductionEngine
 
     def can_accept_order(self, order):
-        recipe = self.recipes[order.recipe]
-        # проверка ингредиентов
-        for ing, qty in recipe["ingredients"].items():
-            if self.inventory.get(ing, 0) < qty:
-                return False
-        return True
-
-    def cook(self, order):
-        recipe = self.recipes[order.recipe]
-        oven = self.equipment["oven_basic"]
-        cook_time = oven.cook_time_min
-        # расход ингредиентов
-        for ing, qty in recipe["ingredients"].items():
-            self.inventory[ing] -= qty
-        # потребление электроэнергии
-        self.electricity_consumed += oven.power_kw * cook_time / 60
-        self.completed_orders += 1
-        return cook_time
+        # Простейшая проверка: есть ли рецепт и доступное оборудование
+        return order.recipe in self.recipes
