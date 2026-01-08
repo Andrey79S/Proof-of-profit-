1b0aa6b7e03a1f44e021f0421e19683c1615a2d1
Andrey79S <phoenixbrite@gmail.com>
2026-01-08 21:09:29 +0200

    Update clock.py

--- Simulator/core/clock.py
+++ Simulator/core/clock.py
@@ -1,10 +1,11 @@
-# core/clock.py
 class Clock:
     def __init__(self):
-        self._time = 0  # минуты
+        self._time = 0  # минуты с начала симуляции
 
     def now(self):
         return self._time
 
-    def tick(self, minutes=1):
+    def tick(self, minutes: int):
+        if minutes < 0:
+            raise ValueError("Время не может идти назад")
         self._time += minutes
