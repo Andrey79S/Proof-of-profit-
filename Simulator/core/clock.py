3d8171fd46b99e34bf6b3494861b57e8a2fb3c2d
Andrey79S <phoenixbrite@gmail.com>
2026-01-09 13:45:05 +0200

    Update clock.py

--- Simulator/core/clock.py
+++ Simulator/core/clock.py
@@ -1,11 +1,23 @@
 class Clock:
-    def __init__(self):
-        self._time = 0  # минуты с начала симуляции
+    def __init__(self, initial_minutes: int = 0):
+        self.current_minutes = initial_minutes
 
-    def now(self):
-        return self._time
+    def now(self) -> int:
+        return self.current_minutes
 
     def tick(self, minutes: int):
         if minutes < 0:
-            raise ValueError("Время не может идти назад")
-        self._time += minutes
+            raise ValueError("Нельзя двигать время назад")
+        self.current_minutes += minutes
+
+    def get_day(self) -> int:
+        return self.current_minutes // (24 * 60)
+
+    def get_hour(self) -> int:
+        return (self.current_minutes % (24 * 60)) // 60
+
+    def __str__(self):
+        d = self.get_day() + 1
+        h = self.get_hour()
+        m = self.current_minutes % 60
+        return f"День {d}, {h:02d}:{m:02d}"
