b9e5da3f63207731f3f7ea8993ceae0e6393255a
Andrey79S <phoenixbrite@gmail.com>
2026-01-09 15:24:08 +0200

    Update offline.py

--- Simulator/app/offline.py
+++ Simulator/app/offline.py
@@ -1,8 +1,5 @@
-from engine.spoilage import apply_spoilage
-from engine.energy import calculate_energy
-
 def apply_offline(pizzeria):
-    delta = 12 * 60  # 12 часов оффлайн
-    calculate_energy(pizzeria, delta)
-    apply_spoilage(pizzeria, pizzeria.clock.now() + delta)
-    pizzeria.clock.tick(delta)
+    print("Оффлайн-процессы...")
+    # Порча, энергия холодильников
+    engine.spoilage.apply_spoilage(pizzeria, pizzeria.clock.now())
+    engine.energy.calculate_daily_energy(pizzeria)
