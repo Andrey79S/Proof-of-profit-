71d398d154f1930db891412913c17cb859ff3dac
Andrey79S <phoenixbrite@gmail.com>
2026-01-10 00:13:48 +0200

    Create ledger.py

---
+++ simulator/accounting/ledger.py
@@ -0,0 +1,14 @@
+class Ledger:
+    def __init__(self):
+        self.revenue = 0.0
+        self.expenses = 0.0
+
+    def add_revenue(self, value: float):
+        self.revenue += value
+
+    def add_expense(self, value: float):
+        self.expenses += value
+
+    @property
+    def profit(self) -> float:
+        return self.revenue - self.expenses
