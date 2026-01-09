66c48068a6d105edf99672bad50e93ee2b5d115b
Andrey79S <phoenixbrite@gmail.com>
2026-01-09 22:38:52 +0200

    Update energy.py

--- Simulator/engine/energy.py
+++ Simulator/engine/energy.py
@@ -16,7 +16,7 @@ class EnergyEngine:
             if eq.type in ["fridge", "proofing_fridge", "table_fridge"]
         )
         daily_kwh = fridge_power * 24
-        daily_cost = daily_kwh * self.pizzeria.economy.get("electricity_price_per_kwh", 0.12)
+        daily_cost = daily_kwh * self.pizzeria.economy.get("electricity_price_per_kwh", 0.2)
 
         self.pizzeria.energy_consumed += daily_kwh
         self.pizzeria.expenses += daily_cost
