a7f5625846a2450ec1ab14d1304f7b5968a0dcc5
Andrey79S <phoenixbrite@gmail.com>
2026-01-10 00:29:02 +0200

    Delete simulator/econom directory

--- simulator/econom/formulas.py
+++
@@ -1,9 +0,0 @@
-def calculate_economics(orders_done: int, config: dict):
-    revenue = orders_done * config["price_per_order"]
-
-    expenses = orders_done * (
-        config["ingredient_cost_per_order"]
-        + config["energy_cost_per_order"]
-    )
-
-    return revenue, expenses
