d565ed4d49ea1eb0824b300721e0fba33c074f35
Andrey79S <phoenixbrite@gmail.com>
2026-01-09 14:02:26 +0200

    Update product.py

--- Simulator/domain/product.py
+++ Simulator/domain/product.py
@@ -1,9 +1,1 @@
-#engine/production.py
 
-from domain.order import Order
-
-def cook_pizza(pizzeria, order: Order):
-if pizzeria.can_accept_order(order):
-pizzeria.cook(order)
-print(f"Приготовлена пицца: {order.recipe}")
-else:
