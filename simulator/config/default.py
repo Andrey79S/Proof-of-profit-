6089ca544bbf85938dd71d9be70bee7402743e0e
Andrey79S <phoenixbrite@gmail.com>
2026-01-10 01:14:16 +0200

    Update default.py

--- simulator/config/default.py
+++ simulator/config/default.py
@@ -1,10 +1,17 @@
 CONFIG = {
     "price_per_order": 10.0,
+
     "ingredient_cost_per_order": 4.0,
     "energy_cost_per_order": 1.0,
 
+    "ingredient_kg_per_order": 0.3,
+    "energy_kwh_per_order": 0.8,
+
+    "ingredient_price_per_kg": 13.0,
+    "energy_price_per_kwh": 1.25,
+
     "base_capacity_per_hour": 10,
 
-    "tap_power": 1,           # заказов за тап
-    "max_reserve": 100,       # лимит резерва
+    "tap_power": 1,
+    "max_reserve": 100,
 }
