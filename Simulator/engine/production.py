2030a88a0189894a778e2688b88fe3dbeaf65e6e
Andrey79S <phoenixbrite@gmail.com>
2026-01-09 15:27:17 +0200

    Update production.py

--- Simulator/engine/production.py
+++ Simulator/engine/production.py
@@ -1,6 +1,11 @@
-def cook_pizza(pizzeria, recipe_name: str):
-    if pizzeria.can_accept_order(recipe_name):
-        pizzeria.cook(recipe_name)
-        print(f"Приготовлена пицца: {recipe_name}")
+# engine/production.py
+
+from domain.pizzeria import Pizzeria
+from domain.order import Order
+
+def cook_pizza(pizzeria: Pizzeria, order: Order):
+    if pizzeria.can_accept_order(order.recipe):
+        pizzeria.cook(order.recipe)
+        pizzeria.clock.tick(15)  # пример: 15 мин на пиццу
     else:
-        print(f"Нельзя приготовить {recipe_name} — недостаточно ресурсов")
+        print(f"Заказ {order.recipe} отклонён — недостаточно ресурсов")
