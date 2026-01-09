65a818ed8ef18882fd826f4e8e2a15b41a21f4c4
Andrey79S <phoenixbrite@gmail.com>
2026-01-09 13:49:03 +0200

    Update staff.py

--- Simulator/domain/staff.py
+++ Simulator/domain/staff.py
@@ -1,20 +1,5 @@
-# domain/staff.py
-
-from dataclasses import dataclass
-
-@dataclass
 class Staff:
-    name: str
-    role: str
-    speed_multiplier: float = 1.0
-    salary_per_hour: float = 0.0
-
-class StaffFactory:
-    @staticmethod
-    def create_from_json(data: dict):  # Теперь принимает dict, а не путь!
-        return Staff(
-            name=data.get("name"),
-            role=data.get("role", "cook"),
-            speed_multiplier=data.get("speed_multiplier", 1.0),
-            salary_per_hour=data.get("salary_per_hour", 0.0)
-        )
+    def __init__(self, name: str, role: str, salary_per_hour: float = 0.0):
+        self.name = name
+        self.role = role
+        self.salary_per_hour = salary_per_hour
