7bea08663203dba488288b7ab54e8e93214e6087
Andrey79S <phoenixbrite@gmail.com>
2026-01-09 14:28:37 +0200

    Update production.py

--- Simulator/engine/production.py
+++ Simulator/engine/production.py
@@ -1,6 +1,10 @@
 #engine/production.py
 
 from domain.order import Order
+def make_dough(pizzeria, amount_kg: float, now: int):
+    batch = DoughBatch(amount_kg, now, now + 2880)  # ← отступ!
+    pizzeria.inventory.add_dough_batch(batch)
+    print(f"Замешено {amount_kg} кг теста")  # ← тоже отступ!
 
 def cook_pizza(pizzeria, order: Order):
 if pizzeria.can_accept_order(order):
