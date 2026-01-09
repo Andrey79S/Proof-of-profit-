5e23ff3fc521f36917b4632ff2ae8ea16b6b3935
Andrey79S <phoenixbrite@gmail.com>
2026-01-09 15:27:45 +0200

    Update dough.py

--- Simulator/engine/dough.py
+++ Simulator/engine/dough.py
@@ -1,4 +1,11 @@
-def make_dough(pizzeria, amount_kg: float, now: int):
+# engine/dough.py
+
+from domain.pizzeria import Pizzeria
+from domain.product import DoughBatch
+
+def make_dough(pizzeria: Pizzeria, amount_kg: float):
+    now = pizzeria.clock.now()
     batch = DoughBatch(amount_kg, now, now + 2880)  # 48 часов
     pizzeria.inventory.add_dough_batch(batch)
+    pizzeria.clock.tick(20)  # время на замес
     print(f"Замешено {amount_kg} кг теста")
