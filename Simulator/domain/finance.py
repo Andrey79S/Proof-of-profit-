54b609f82a87c4a96cd1bbac8b38659db09a483a
Andrey79S <phoenixbrite@gmail.com>
2026-01-09 15:25:54 +0200

    Update finance.py

--- Simulator/domain/finance.py
+++ Simulator/domain/finance.py
@@ -1,10 +1,19 @@
-#domain/finance.py
+# domain/finance.py
 
 class Finance:
-def init(self):
-self.revenue = 0.0
-self.expenses = 0.0
-self.losses = 0.0
+    def __init__(self):
+        self.revenue = 0.0
+        self.expenses = 0.0
+        self.losses = 0.0
 
-def profit(self) -> float:  
-    return self.revenue - self.expenses - self.losses
+    def add_revenue(self, amount: float):
+        self.revenue += amount
+
+    def add_expense(self, amount: float):
+        self.expenses += amount
+
+    def add_loss(self, amount: float):
+        self.losses += amount
+
+    def net_profit(self) -> float:
+        return self.revenue - self.expenses - self.losses
