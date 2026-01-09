29fa01bfde613ba9de2b7832dc4c1e20fbdb61d8
Andrey79S <phoenixbrite@gmail.com>
2026-01-10 01:36:35 +0200

    Update engine.py

--- simulator/simulation/engine.py
+++ simulator/simulation/engine.py
@@ -7,10 +7,9 @@ def simulate_time(pizzeria, config, hours: float):
     if executed <= 0:
         return
 
-    revenue, expenses, flows = calculate_economics(executed, config)
+    revenue, expenses, flows = calculate_economics(executed, pizzeria, config)
 
     pizzeria.ledger.add_revenue(revenue)
     pizzeria.ledger.add_expense(expenses)
-
     pizzeria.ledger.ingredients_used += flows["ingredients_kg"]
     pizzeria.ledger.energy_used += flows["energy_kwh"]
