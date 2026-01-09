2fab6c2bc13ca7bcb5b47fac5960e44efca5884f
Andrey79S <phoenixbrite@gmail.com>
2026-01-09 13:51:20 +0200

    Create finance.py

---
+++ Simulator/domain/finance.py
@@ -0,0 +1,10 @@
+domain/finance.py
+
+class Finance:
+def init(self):
+self.revenue = 0.0
+self.expenses = 0.0
+self.losses = 0.0
+
+def profit(self) -> float:  
+    return self.revenue - self.expenses - self.losses
