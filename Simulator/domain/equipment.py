2664c3e4d6d967805f66679a9099db5bb66f3722
Andrey79S <phoenixbrite@gmail.com>
2026-01-09 15:19:51 +0200

    Update equipment.py

--- Simulator/domain/equipment.py
+++ Simulator/domain/equipment.py
@@ -1,8 +1,6 @@
 class Equipment:
-    def __init__(self, data: dict):
-        self.name = data["name"]
-        self.type = data["type"]
-        self.power_kw = data.get("power_kw", 0.0)
-        self.capacity = data.get("capacity", 0.0)
-        self.min_batch = data.get("min_batch_kg", 0.0)
-        self.max_batch = data.get("max_batch_kg", 0.0)
+    def __init__(self, name: str, type: str, power_kw: float = 0.0, capacity: float = 0.0):
+        self.name = name
+        self.type = type
+        self.power_kw = power_kw
+        self.capacity = capacity
