bf42fc109c28c9a660e6a68c1445d7f022234017
Andrey79S <phoenixbrite@gmail.com>
2026-01-10 02:16:18 +0200

    Update ledger.py

--- simulator/accounting/ledger.py
+++ simulator/accounting/ledger.py
@@ -4,12 +4,17 @@ class Ledger:
         self.expenses = 0.0
         self.ingredients_used = 0.0
         self.energy_used = 0.0
+        self.pizza_details = {}  # хранит количество пицц по типам
 
-    def add_revenue(self, value: float):
-        self.revenue += value
+    def add(self, econ_data: dict):
+        self.revenue += econ_data["revenue"]
+        self.expenses += econ_data["expenses"]
+        self.ingredients_used += econ_data["ingredients_used_kg"]
+        self.energy_used += econ_data["energy_used_kwh"]
 
-    def add_expense(self, value: float):
-        self.expenses += value
+        # обновляем количество пицц
+        for pizza_type, qty in econ_data["details"]["per_pizza_type"].items():
+            self.pizza_details[pizza_type] = self.pizza_details.get(pizza_type, 0) + qty
 
     @property
     def profit(self) -> float:
